0f432c6b6760ac441019d56e5171f9da9207cf45
Alex Ghiurau <alexg@paymoapp.com>
2017-03-29 11:01:27 +0300

    update drive.js

--- drive.js
+++ drive.js
@@ -69,18 +69,18 @@ function onAddFilesFromGdriveBtnClick( el ) {
 	}
 }
 
-function loadScript( url, callback ) {
-	// Adding the script tag to the head as suggested before
-	var head = document.getElementsByTagName( 'head' )[0];
-	var script = document.createElement( 'script' );
-	script.type = 'text/javascript';
-	script.src = url;
-
-	// Then bind the event to the callback function.
-	// There are several events for cross browser compatibility.
-	script.onreadystatechange = callback;
-	script.onload = callback;
-
-	// Fire the loading
-	head.appendChild( script );
+function loadScript(sScriptSrc, oCallback) {
+	var oHead = document.getElementById('head')[0];
+	var oScript = document.createElement('script');
+	oScript.type = 'text/javascript';
+	oScript.src = sScriptSrc;
+// most browsers
+	oScript.onload = oCallback;
+// IE 6 & 7
+	oScript.onreadystatechange = function() {
+		if (this.readyState == 'complete') {
+			oCallback();
+		}
+	};
+	oHead.appendChild(oScript);
 }
